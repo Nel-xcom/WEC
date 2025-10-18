7f3f0f578dec6034c6c74482c4ff918ef4832cfd
Santiago Campanello <terricolaproject@gmail.com>
2025-10-18 02:03:22 -0300

    Fix image paths for GitHub Pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -83,7 +83,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "6c14858378ceceaaaaf22c674bace39b
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "0dbe7bfeeb73a1686d9619200972c6eb",
+"flutter_bootstrap.js": "bf2c5d121e311bc793bf6c638bfe0da0",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
